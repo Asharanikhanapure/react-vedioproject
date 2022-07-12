ee85b26d874cba2ec4250ebddb948ccb8b4a733a
asharani <asharani@trempplin.com>
2022-07-12 13:18:52 +0530

    completed

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,21 +1,41 @@
 import React,{Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
-//import Greet from './components/Greet'
-// import Welcome from './components/Welcome'
-// import Hello from './components/Hello';
-// import Message from './components/Message';
-// import Counter from './components/Counter';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from './components/ClassClick';
-// import EventBind from './components/EventBind';
-// import ParentComponent from './components/ParentComponent';
-//import UserGreeting from './components/UserGreeting';
-//import NameList from './components/NameList';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
-import './appStyles.css';
 import styles from './appStyles.module.css';
+import Form from './components/Form';
+import LifeCycleA from './components/LifeCycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+import Purecomp from './components/PureComp';
+import ParentComp from './components/ParentComp';
+import PureComp from './components/PureComp';
+import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import ClickCounteTwor from './components/ClickCounteTwor';
+import User from './components/User';
+import Counter123 from './components/Counter123';
+import { UserProvider } from './components/UserContext';
+import ComponentC from './components/ComponentC';
+
 
 
 
@@ -26,9 +46,70 @@ class App extends Component {
  return (
 
     <div className="App">
-      <h1 className='error'>Error</h1>
+      <UserProvider value="asharani"></UserProvider>
+      <ComponentC/>
+       <Form />
+       
+       {/* <Counter123 
+        render={(count,incrementCount)=>
+        (<ClickCounteTwor count={count} incrementCount={incrementCount}/>
+        )}
+        />
+        
+        <Counter123 
+        render={(count,incrementCount)=>
+        (<HoverCountertwo count={count} incrementCount={incrementCount}/>
+        )}
+        />
+         */}
+
+         {/* 
+  <User render={(isLoggedIn)=>isLoggedIn?'Anjana':'guest'}/>
+        <ClickCounteTwor/>
+        <HoverCountertwo/> */}
+       {/* <ClickCounter name="Anjana" />
+
+<HoverCounter/> */}
+
+        {/* <ClickCounter/>
+      <ErrorBoundary>
+    <Hero heroName="asha"/></ErrorBoundary>
+    <ErrorBoundary> <Hero heroName="pallavi"/></ErrorBoundary>
+    <ErrorBoundary><Hero heroName="anjana"/>
+    </ErrorBoundary> */}
+    
+    {/* <Hero heroName="asharani"/> */}
+       {/* <PortalDemo/> */}
+        
+        {/* <FRParentInput/> */}
+        {/* <FocusInput/>
+        <RefsDemo/> */}
+        {/* <ParentComp/> */}
+        {/* <PureComp/> */}
+        {/* <Table/>
+       <FragmentDemo/> 
+          <Form/>
+          <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success
+        </h1>
+         <Inline/>  
+         <LifeCycleA /> 
+         <Stylesheet primary={true}/>
+         <NumberList/> 
+        
+         <UserGreeting/> 
+         <ParentComponent/>
+          <EventBind/>
+        <FunctionClick/>
+        <ClassClick/>  
+
+ <Counter/> 
+
+
+
+      {/* <h1 className='error'>Error</h1>
       <h1 classname={styles.success}>Success</h1>
-      <Inline />
+      <Inline /> */}
 
       {/* <Stylesheet primary={true}/> */}
       
@@ -38,10 +119,7 @@ class App extends Component {
       
 
       {/*<ParentComponent />*/}
-     {/*<FunctionClick />
-      <ClassClick />
- <EventBind />*/}
-      {/*<Counter />*/}
+     
      {/*Greet name="Anjana" cityname="Bangalore"/>
       <p>
         This is a Trempplin company
